b642977cee5a995b59489f4786b97a03fd1085f8
Dmytro Yashkir <dima.yashkir@gmail.com>
2012-05-04 10:49:14 -0400

    -1 char

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,1 +1,1 @@
-main(n){for(;n<101;n++)printf("%s%s%0.0d",n%3==0?"Fizz":"",n%5==0?"Buzz":"",n%5&&n%3?n:0);}
+main(n){for(;n<101;n++)printf("%s%s%.0d",n%3==0?"Fizz":"",n%5==0?"Buzz":"",n%5&&n%3?n:0);}
